982c9a655275a27412a4ad21c6704e1b6973aedf
rajkhandeka <rajkhandeka2003@gmail.com>
2024-06-11 15:05:00 +0530

    Travel commit

---
+++ src/data.js
@@ -0,0 +1,55 @@
+
+
+const data = [
+    {
+        id:1,
+        name:"Agra",
+        info:"agra is a city in northern India, The city is famous for being the capital of the Mughal emperors from 1526 to 1658.Located on the banks of Yamuna, Agra is best known for the iconic Taj Mahal. However, the appeal of this historic city is not limited to this magnificent monument alone. There's a lot to explore and experience, from monuments and mausoleums to parks and bazaars.",
+        image: "https://th.bing.com/th/id/OIP.C7T8T10Hv9qan1MXqARg3AHaFS?w=190&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+        price: "35,758",
+    },
+    {
+        id:2,
+        name:"Jaipur",
+        info:"Jaipur is the capital city of the Indian state of Rajasthan, know as Pink City.Jaipur is also known as the Pink City, due to the dominant colour scheme of its buildings. It is also known as the Paris of India, and C. V. Raman called it the Island of Glory. It is located 268 km (167 miles) from the national capital New Delhi. ",
+        image: "https://th.bing.com/th?id=OIP._613i1YJqNE-o2iDIdAZ4gHaJI&w=137&h=170&rs=1&qlt=80&o=6&dpr=1.3&pid=3.1",
+        price: "82,560",
+    },
+    {
+        id:3,
+        name:"Goa",
+        info:"Goa is visited by large numbers of international and domestic tourists each year because of its white-sand beaches, active nightlife, places of worship, and World Heritage-listed architecture.The state of Goa, in India, is famous for its beaches and places of worship. Tourism is its primary industry, and is generally focused on the coastal areas of Goa, with decreased tourist activity inland.",
+        image: "https://th.bing.com/th?id=ODL.10985bdb06097453bca31a3a55805e01&w=164&h=91&c=10&rs=1&qlt=99&o=6&dpr=1.3&pid=13.1",
+        price: "29,695",
+    },
+    {
+        id:4,
+        name:"Varanasi",
+        info:"Varanasi is a city on the banks of the river Ganges in Uttar Pradesh, India, that is a sacred and ancient centre of Hinduism, Jainism, Buddhism, and Ravidassia.Hindus believe that one who is graced to die on the land of Varanasi would attain salvation and freedom from the cycle of birth and re-birth. Abode of Lord Shiva and Parvati, the origins of Varanasi are yet unknown.",
+        image: "https://th.bing.com/th?id=OIP.vGnxhypcf2P8wZmLQqwEOAHaE6&w=200&h=132&rs=1&qlt=80&o=6&dpr=1.3&pid=3.1",
+        price: "31,095",
+    },
+    {
+        id:5,
+        name:"Darjeeling",
+        info:"Darjeeling is a picturesque hill station in West Bengal, India, known as the Queen of the Hills and the Champagne of Teas.Industry. Tea and Darjeeling are synonymous. Darjeeling Tea is world renowned for its flavour, which is unequaled by other tea producing areas not only of India but also of the world. Darjeeling still manufactures the tea by the original methods known as the 'orthodox' method.",
+        image: "https://th.bing.com/th?id=OIP.PX1m9q0ZJVw6kdaht9YoNQHaER&w=218&h=126&rs=1&p=0&o=6&dpr=1.3&pid=Stories",
+        price: "78,595",
+    },
+    {
+        id:6,
+        name:"Jaisalmer",
+        info:"Jaisalmer (pronunciation) , nicknamed The Golden city, is a city in the Indian state of Rajasthan, located 575 kilometres (357 mi) west of the state capital Jaipur yellow sandstone walls are a tawny lion colour during the day, fading to honey-gold as the sun sets, thereby camouflaging the fort in the yellow desert. For this reason it is also known as the Swarn Durg, Sonar Quila or Golden Fort.",
+        image: "https://th.bing.com/th?id=OSK.HERO-gBJ5xJydzJ0-clHP5B7hfWrkvHAdmrvw6pj7YJU4BQ&w=296&h=176&c=1&rs=2&o=6&dpr=1.3&pid=SANGAM",
+        price: "68,595",
+    },
+    {
+        id:7,
+        name:"Kochi",
+        info:"Kochi is also called Cochin, “Cochin” is actually a European variant of “Kochi” and The first European church was built here .Also referred to as Cochin, Kochi is an enchanting city situated on the southwest coast of India in the state of Kerala. Popularly known as 'Queen of the Arabian Sea', it is a flourishing port city showcasing a rich blend of mesmerising natural beauty and vibrant culture.",
+        image: "https://th.bing.com/th?id=OIP.sAQHNfmL9S9vHB1MyYo9OQHaFj&w=200&h=149&rs=1&qlt=80&o=6&dpr=1.3&pid=3.1",
+        price: "68,900",
+    },
+
+];
+export default data;
